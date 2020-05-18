032aa1a7b0ff7033962e508ca10ea40c2587ff5c
Yannick <20306025+yandeu@users.noreply.github.com>
2020-05-19 01:56:26 +0200

    Use jest for testing

--- test/test.js
+++ test/test.js
@@ -1,56 +1,57 @@
 require('../lib/index.js')
 const Phaser = require('phaser')
 
-class Player extends Phaser.Physics.Arcade.Sprite {
-  constructor(scene, x, y) {
-    // pass empty string for the texture
-    super(scene, x, y, '')
+it('should render without errors', done => {
+  class Player extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y) {
+      // pass empty string for the texture
+      super(scene, x, y, '')
 
-    scene.add.existing(this)
-    scene.physics.add.existing(this)
+      scene.add.existing(this)
+      scene.physics.add.existing(this)
 
-    // set the width and height of the sprite as the body size
-    this.body.setSize(32, 48)
-  }
-}
-
-class MainScene extends Phaser.Scene {
-  constructor() {
-    super('MainScene')
-    this.logged = false
-    this.player
-  }
-
-  create() {
-    console.log('create')
-    this.player = new Player(this, 100, 100)
-  }
-
-  update(time, delta) {
-    if (!this.logged && this.player.y > 200) {
-      this.logged = true
-      console.log('player', this.player.y.toFixed())
-      console.log('update')
-
-      // exit the node process
-      process.exit()
+      // set the width and height of the sprite as the body size
+      this.body.setSize(32, 48)
     }
   }
-}
 
-const config = {
-  type: Phaser.HEADLESS,
-  width: 1280,
-  height: 720,
-  banner: false,
-  audio: false,
-  scene: [MainScene],
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 1200 },
+  class MainScene extends Phaser.Scene {
+    constructor() {
+      super('MainScene')
+      this.logged = false
+      this.player
+    }
+
+    create() {
+      console.log('create')
+      this.player = new Player(this, 100, 100)
+    }
+
+    update(time, delta) {
+      if (!this.logged && this.player.y > 200) {
+        this.logged = true
+        console.log('player', this.player.y.toFixed())
+        console.log('update')
+
+        done()
+      }
+    }
+  }
+
+  const config = {
+    type: Phaser.HEADLESS,
+    width: 1280,
+    height: 720,
+    banner: false,
+    audio: false,
+    scene: [MainScene],
+    physics: {
+      default: 'arcade',
+      arcade: {
+        gravity: { y: 1200 },
+      },
     },
-  },
-}
+  }
 
-new Phaser.Game(config)
+  new Phaser.Game(config)
+})
